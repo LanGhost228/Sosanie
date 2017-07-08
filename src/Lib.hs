c1b6b6ffa37ae2640c834d564631a72e26b6867b
Your Name <you@example.com>
2017-07-08 13:53:59 +0300

    dodelal

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,14 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+	ilososCat,
+	avg,
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data Ilosos = G1 | G2 | G3 deriving (Show,Read)
+ilososCat :: [Float] -> Ilosos
+ilososCat x = G3
+
+avg x = sum x  / (fromIntegral $ length x)
